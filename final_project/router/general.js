1f4acf93c4355c2c0c59fe86afbe0ad29a559e0c
mkhalil12 <mohebkhalil97@gmail.com>
2025-02-04 19:21:08 -0500

    initial commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,29 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    // Filter the books array to find books with isbn that matches the extracted isbn parameter
+    let filtered_books = books.filter((book) => book.isbn === isbn);
+    // Send the filtered_books array as the response to the client
+    res.send(JSON.stringify({filtered_books}, null, 4));
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/:author', (req, res) =>{
+    const author = req.params.author;
+    let filtered_books = books.filter((books) => books.author === author);
+
+    if (filtered_books) {
+        res.send(JSON.stringify(book, null, 4));
+    } else {
+        res.send(`Book with author name ${author} not found.`);
+        console.log(filtered_books);
+    }
 });
 
 // Get all books based on title
